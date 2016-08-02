f11a12cadcb8a65c033fae1e5a01466d61775b02
sheltowt <williamshelton@Williams-Mac-mini.local>
2016-08-02 15:24:29 -0400

    adding updates

---
+++ scripts/summary_contains.py
@@ -0,0 +1,24 @@
+import json
+import pymongo
+import glob
+import sys
+sys.path.append('../')
+sys.path.append('../helpers')
+from import_config import load_config
+from save_to_csv import export__all_data
+from pymongo import MongoClient
+
+#load in config file
+
+config = load_config()
+
+#initialize connection to mongodb
+
+client = MongoClient(config["database"]["connection_url"])
+
+db = client[config['database']['database_name']]
+conn = db[config['database']['collection_name']]
+
+data = conn.find({"action.misuse.vector": "LAN access"})
+
+export__all_data(data, "action_vector.csv")
