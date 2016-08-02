67ae39a5f1859020dc78941134aaac46d42103c8
sheltowt <williamshelton@Williams-Mac-mini.local>
2016-08-02 15:22:21 -0400

    adding updates

--- helpers/save_to_csv.py
+++ helpers/save_to_csv.py
@@ -5,15 +5,9 @@ def export__all_data(data, file_name):
 
 	file = unicodecsv.writer(open("../tmp/"+file_name, "wb"))
 	for row in data:
-		file.writerow(
+		file.writerow([
 				row["action"]["misuse"]["variety"],
 				row["action"]["misuse"]["vector"],
 				row["discovery_method"],
-				row["incident_id"],
-				row["timeline"]["incident"]["day"],
-				row["timeline"]["incident"]["month"],
-				row["timeline"]["incident"]["year"],
-				row["summary"],
-				row["state"],
-				row["victim_id"],
-			)
+				row["incident_id"]
+			])
