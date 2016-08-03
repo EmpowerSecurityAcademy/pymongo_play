89684004bc0906251974380e9091eabaab44c0e4
sheltowt <sheltowt@gmail.com>
2016-08-03 15:05:44 -0400

    adding updates

--- verizon_scripts/summary_contains.py
+++ verizon_scripts/summary_contains.py
@@ -4,7 +4,7 @@ import glob
 import sys
 sys.path.append('../helpers')
 from import_config import load_config
-from save_to_csv import export__all_data
+from save_to_json import json_export
 from pymongo import MongoClient
 
 #load in config file
@@ -18,6 +18,6 @@ client = MongoClient(config["database"]["connection_url"])
 db = client[config['database']['database_name']]
 conn = db[config['database']['collection_name']]
 
-data = conn.find({"action.misuse.vector": "LAN access"})
+data = conn.find({"summary": {'$regex':'patient'}})
 
-export__all_data(data, "action_vector.csv")
+json_export(data, "../tmp/summary_contains.json")
--- verizon_scripts/unique_vectors.py
+++ verizon_scripts/unique_vectors.py
@@ -5,7 +5,7 @@ import sys
 sys.path.append('../')
 sys.path.append('../helpers')
 from import_config import load_config
-from save_to_csv import export__all_data
+from save_to_json import json_export
 from pymongo import MongoClient
 
 #load in config file
@@ -19,6 +19,6 @@ client = MongoClient(config["database"]["connection_url"])
 db = client[config['database']['database_name']]
 conn = db[config['database']['collection_name']]
 
-data = conn.find({"action.misuse.vector": "LAN access"})
+data = conn.distinct("victim.victim_id")
 
-export__all_data(data, "action_vector.csv")
+json_export(data, "../tmp/unique_vector.json")
