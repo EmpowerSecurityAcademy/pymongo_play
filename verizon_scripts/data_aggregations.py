5ea4d41b843225dd9458cd24493f69683f774c43
sheltowt <sheltowt@gmail.com>
2016-08-03 13:37:30 -0400

    adding updates

--- verizon_scripts/data_aggregations.py
+++ verizon_scripts/data_aggregations.py
@@ -1,1 +1,25 @@
 import pymongo
+import sys
+sys.path.append('../helpers')
+from import_config import load_config
+from save_to_json import json_export
+from pymongo import MongoClient
+
+# load in config file
+
+config = load_config()
+
+# initialize connection to mongodb
+
+client = MongoClient(config["database"]["connection_url"])
+
+db = client[config['database']['database_name']]
+conn = db[config['database']['collection_name']]
+
+# pull all tweets from mongodb
+
+data = conn.find({"action.misuse.vector": "LAN access"})
+
+# export them to a csv file
+
+json_export(data, "../tmp/data_aggregations.json")
